1d40cfce8b477b97a35856858217da5b1c62f281
bao ann <baoanh1buinguyen@gmail.com>
2023-10-23 16:53:59 +0800

    ADD options

--- app/api/openai/route.js
+++ app/api/openai/route.js
@@ -1,5 +1,4 @@
 import OpenAI from 'openai';
-import Cors from 'cors';
 import { NextResponse } from 'next/server';
 
 export const revalidate = 0;
@@ -10,16 +9,7 @@ const openai = new OpenAI({
     apiKey: process.env['OPEN_AI_API_KEY'],
 });
 
-// Create a CORS handler
-const cors = Cors({
-    origin: '*', // Allow requests from this origin
-    methods: ['POST'], // Define the allowed HTTP methods
-});
-
-export const POST = async (request, response) => {
-    // Apply CORS middleware to handle preflight requests
-    await cors(request, response);
-
+export async function POST(request) {
     try {
         const { messages } = await request.json();
         console.log(messages);
@@ -67,4 +57,48 @@ export const POST = async (request, response) => {
             }
         );
     }
-};
+}
+
+export async function OPTIONS(request) {
+    try {
+        const allowedOrigin = request.headers.get('origin');
+        return new Response(
+            { jsonBody: { error: true, msg: 'OPTIONS' } },
+            {
+                status: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': allowedOrigin || '*',
+                    'Access-Control-Allow-Methods':
+                        'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers':
+                        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                },
+            }
+        );
+    } catch (error) {
+        if (error instanceof OpenAI.APIError) {
+            console.error(error.status); // e.g. 401
+            console.error(error.message); // e.g. The authentication token you passed was invalid...
+            console.error(error.code); // e.g. 'invalid_api_key'
+            console.error(error.type); // e.g. 'invalid_request_error'
+        } else {
+            // Non-API error
+            console.log(error);
+        }
+        return new Response(
+            {
+                jsonBody: { error: true, msg: error.message },
+            },
+            {
+                status: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': allowedOrigin || '*',
+                    'Access-Control-Allow-Methods':
+                        'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers':
+                        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                },
+            }
+        );
+    }
+}
