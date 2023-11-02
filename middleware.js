4c1e098f62b53845d54fccc3b9df8b2baa55ce4a
bao ann <baoanh1buinguyen@gmail.com>
2023-11-03 04:06:16 +0800

    middleware fix

--- middleware.js
+++ middleware.js
@@ -21,18 +21,18 @@ export function middleware(req) {
     // add it to the 'Access-Control-Allow-Origin' header
     if (allowedOrigins.includes(origin)) {
         res.headers.append('Access-Control-Allow-Origin', origin);
+        // add the remaining CORS headers to the response
+        res.headers.append('Access-Control-Allow-Credentials', 'true');
+        res.headers.append(
+            'Access-Control-Allow-Methods',
+            'GET,DELETE,PATCH,POST,PUT'
+        );
+        res.headers.append(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        );
     }
 
-    // add the remaining CORS headers to the response
-    res.headers.append('Access-Control-Allow-Credentials', 'true');
-    res.headers.append(
-        'Access-Control-Allow-Methods',
-        'GET,DELETE,PATCH,POST,PUT'
-    );
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
     return res;
 }
 
