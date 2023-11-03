01dd0994fa49ea4617cf1894106c5edf253ab725
bao ann <baoanh1buinguyen@gmail.com>
2023-11-03 12:02:45 +0800

    hopefully it works

--- middleware.js
+++ middleware.js
@@ -6,20 +6,23 @@ const allowedOrigins = [
     'https://vanghoa.github.io',
     'http://localhost:5501',
     'http://127.0.0.1:5501',
+    'http://localhost:5502',
+    'http://127.0.0.1:5502',
 ];
 
 export function middleware(req) {
     // retrieve the current response
     console.log('middleware <3');
-    const res = NextResponse.next();
 
     // retrieve the HTTP "Origin" header
     // from the incoming request
     const origin = req.headers.get('origin');
+    console.log(origin);
 
     // if the origin is an allowed one,
     // add it to the 'Access-Control-Allow-Origin' header
     if (allowedOrigins.includes(origin)) {
+        const res = NextResponse.next();
         res.headers.append('Access-Control-Allow-Origin', origin);
         // add the remaining CORS headers to the response
         res.headers.append('Access-Control-Allow-Credentials', 'true');
@@ -31,14 +34,13 @@ export function middleware(req) {
             'Access-Control-Allow-Headers',
             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
         );
+        return res;
     } else {
         return NextResponse.json(
             { message: 'Aha! Not so fast.' },
             { status: 401 }
         );
     }
-
-    return res;
 }
 
 // specify the path regex to apply the middleware to
